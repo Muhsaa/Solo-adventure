54b6bad54e5c352180c3dfbaacad81fcb8668178
Muhsaa <muhammad.saad2@elev.ga.ntig.se>
2025-03-20 12:57:40 +0100

    Add files via upload

--- Soloäventyr.py
+++ Soloäventyr.py
@@ -21,9 +21,10 @@ def visa_menyn():
 
 # Funktion för att starta spelet
 def starta_spel():
+    global halsa,attack,smidighet,plats,inventarium
     print_slow("Välj din karaktär:")
     print_slow("1. Krigare")
-    print_slow("2. Magiker")
+    print_slow("2. Monk")
     print_slow("3. Tjuv")
 
 
@@ -32,19 +33,16 @@ def starta_spel():
         if karaktar_val == "1":
             halsa = 120
             attack = 20
-            magi = 0
-            smidighet = 5
+            smidighet = 25
             break
         elif karaktar_val == "2":
             halsa = 80
             attack = 10
-            magi = 25
             smidighet = 10
             break
         elif karaktar_val == "3":
             halsa = 100
             attack = 15
-            magi = 5
             smidighet = 20
             break
         else:
@@ -53,28 +51,36 @@ def starta_spel():
     print_slow("Du vaknar i en mörk grotta.")
     plats = "grottan"
     inventarium = []
-    hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+    hantera_spelet(plats, halsa, inventarium, attack, smidighet)
 
 # Funktion för att hantera spelet
-def hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet):
+def hantera_spelet(plats, halsa, inventarium, attack, smidighet):
     if plats == "grottan":
         print_slow("Du ser två vägar framåt.")
-        val = input("Vill du gå vänster (v) eller höger (h)? ")
+        print_slow("1.Vänster (v)\n"
+        "2.Höger (h)? \n"
+        "3.Spara spelet (s)")
+        val = input("Skriv din val: ")
         if val == "v":
             plats = "skattkammaren"
             print_slow("Du går vänster.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
+        elif val == "s":
+            spara_spelet()
         elif val == "h":
             plats = "mork_gang"
             print_slow("Du går höger.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         else:
             print_slow("Ogiltigt val.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
     elif plats == "skattkammaren":
         print_slow("Du hittar en skattkista! Men en goblin vaktar den.")
-        val = input("Vill du slåss (s) eller gå tillbaka (g)? ")
-        if val == "s":
+        print_slow("1.Slåss (a)\n"
+        "2.Gå tillbaka (g)\n"
+        "3.Spara spelet (s)")
+        val = input("Skriv din val: ")
+        if val == "a":
             print_slow("Du slåss mot goblinen!")
             halsa = halsa - (20 - smidighet) # Smidighet minskar skada.
             print_slow(f"Din hälsa: {halsa}")
@@ -85,14 +91,16 @@ def hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet):
                 inventarium.append("guld")
                 print_slow("Du hittade guld!")
                 plats = "grottan"
-                hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+                hantera_spelet(plats, halsa, inventarium, attack, smidighet)
+        elif val == "s":
+            spara_spelet()
         elif val == "g":
             plats = "grottan"
             print_slow("Du går tillbaka.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         else:
             print_slow("Ogiltigt val.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
     elif plats == "mork_gang":
         print_slow("Du går in i en mörk gång. Du snubblar och tappar 10 hälsa.")
         halsa = halsa - 10
@@ -102,61 +110,62 @@ def hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet):
         else:
             plats = "stenbron"
             print_slow("Du hittar en stenbro.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
     elif plats == "stenbron":
         print_slow("Du står på en smal stenbro över en avgrund. Vad gör du?")
         val = input("Gå över (ö) eller gå tillbaka (g)? ")
         if val == "ö":
             plats = "hemlig_kammare"
             print_slow("Du går över bron.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         elif val == "g":
             plats = "mork_gang"
             print_slow("Du går tillbaka.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         else:
             print_slow("Ogiltigt val.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
     elif plats == "hemlig_kammare":
         print_slow("Du hittar en hemlig kammare med en gammal bok.")
         val = input("Läsa boken (l) eller gå tillbaka (g)? ")
         if val == "l":
-            print_slow("Du läser boken och hittar en magisk formel!")
-            inventarium.append("magisk_formel")
+            print_slow("Du läser boken och hittar en sk formel!")
+            inventarium.append("sk_formel")
             plats = "avgrunden"
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         elif val == "g":
             plats = "stenbron"
             print_slow("Du går tillbaka.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         else:
             print_slow("Ogiltigt val.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
     elif plats == "avgrunden":
         print_slow("Du står vid en djup avgrund. Du ser en liten stig på andra sidan.")
         val = input("Använd formeln (a) eller gå tillbaka (g)? ")
-        if val == "a" and "magisk_formel" in inventarium:
+        if val == "a" and "sk_formel" in inventarium:
             plats = "utgangen"
             print_slow("Du använder formeln och skapar en tillfällig bro!")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         elif val == "g":
             plats = "hemlig_kammare"
             print_slow("Du går tillbaka.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
         else:
             print_slow("Ogiltigt val eller du har inte formeln.")
-            hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+            hantera_spelet(plats, halsa, inventarium, attack, smidighet)
     elif plats == "utgangen":
         print_slow("Du ser utgången! Du har klarat äventyret!")
         print_slow("Grattis!")
         return
 
 # Funktion för att spara spelet
-def spara_spelet(plats, halsa, inventarium, attack, magi, smidighet):
+def spara_spelet(plats, halsa, inventarium, attack, smidighet):
     try:
         with open("savegame.txt", "w") as fil:
-            fil.write(f"{plats}\n{halsa}\n{inventarium}\n{attack}\n{magi}\n{smidighet}")
+            fil.write(f"{plats}\n{halsa}\n{inventarium}\n{attack}\n{smidighet}")
         print_slow("Spelet sparades!")
+        visa_menyn()
     except:
         print_slow("Kunde inte spara spelet.")
 
@@ -168,10 +177,9 @@ def ladda_spelet():
             halsa = int(fil.readline().strip())
             inventarium = eval(fil.readline().strip())
             attack = int(fil.readline().strip())
-            magi = int(fil.readline().strip())
             smidighet = int(fil.readline().strip())
         print_slow("Spelet laddades!")
-        hantera_spelet(plats, halsa, inventarium, attack, magi, smidighet)
+        hantera_spelet(plats, halsa, inventarium, attack, smidighet)
     except FileNotFoundError:
         print_slow("Ingen sparfil hittades.")
     except:
